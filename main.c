1bd9860a062def4cf4877e884175370957a3225a
cakoth77 <christineberny77@gmail.com>
2023-04-13 00:02:21 -0700

    EOF

--- main.c
+++ main.c
@@ -12,13 +12,25 @@ char *prompt = "(simpleshell) $ ";
 /* variables for typed info and size allocation */
 char *typed;
 size_t m = 0;
+ssize_t mchars_re = 0;
 /* void variables */
 (void)bc;
 (void)argv;
+/* creating a loop */
+while (1)
+{
 printf("%s", prompt);
+mchars_re = getline(&typed, &m, stdin);
+/* check if function failed or EOF(Ctrl+D) */
+if (mchars_re == -1)
+{
+	printf("Exiting simpleshell....\n");
+	return (-1);
+}
 /* to get what user types */
-getline(&typed, &m, stdin);
+/* getline(&typed, &m, stdin); */
+printf("%s\n", typed);
 free(typed);
+}
 return (0);
 }
-
