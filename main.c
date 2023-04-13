5165bc4ce3599ca79b9dd9f69a73a81b1ff66711
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-04-13 04:02:27 -0700

    simple shell

--- main.c
+++ main.c
@@ -1,24 +1,32 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * main - printing a prompt and reading
- * @bc: integer
- * @argv: arguments
- * Return: Always 0
- */
-int main(int bc, char **argv)
-{
-char *prompt = "(simpleshell) $ ";
-/* variables for typed info and size allocation */
-char *typed;
-size_t m = 0;
-/* void variables */
-(void)bc;
-(void)argv;
-printf("%s", prompt);
-/* to get what user types */
-getline(&typed, &m, stdin);
-free(typed);
-return (0);
-}
 
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints a prompt and reads a line of input from
+ *              the user using the getline() function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int ac, char **argv)
+{
+        char *prompt = "(Gitshell) $ ";
+        char *lineptr;
+        size_t n = 0;
+
+/* declaring void variables */
+        (void)ac; (void)argv;
+
+        for (int i = 0; prompt[i] != '\0'; i++)
+        {
+                putchar(prompt[i]);
+        }
+
+        getline(&lineptr, &n, stdin);
+        printf("%s\n", lineptr);
+
+        free(lineptr);
+        return (0);
+}
