ab75ac97d0fd3c70395b77d19d0001d8577f5728
cakoth77 <christineberny77@gmail.com>
2023-04-14 04:43:39 -0700

    main

--- main.c
+++ main.c
@@ -1,36 +1,43 @@
-#include "main.h"
-#include <stdio.h>
+#include "shell.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 /**
- * main - printing a prompt and reading
- * @bc: integer
- * @argv: arguments
- * Return: Always 0
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 0 on success, 1 on error
  */
-int main(int bc, char **argv)
+int main(int ac, char **av)
 {
-char *prompt = "(simpleshell) $ ";
-/* variables for typed info and size allocation */
-char *typed;
-size_t m = 0;
-ssize_t mchars_re = 0;
-/* void variables */
-(void)bc;
-(void)argv;
-/* creating a loop */
-while (1)
+info_t info[] = { INFO_INIT };
+int fd = 2;
+fd = dup(STDERR_FILENO);
+if (fd == -1)
+return (EXIT_FAILURE);
+if (ac == 2)
 {
-printf("%s", prompt);
-mchars_re = getline(&typed, &m, stdin);
-/* check if function failed or EOF(Ctrl+D) */
-if (mchars_re == -1)
+fd = open(av[1], O_RDONLY | O_CREAT, 0644);
+if (fd == -1)
 {
-	printf("Exiting simpleshell....\n");
-	return (-1);
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+{
+write(STDERR_FILENO, av[0], strlen(av[0]));
+write(STDERR_FILENO, ": 0: Can't open ", 17);
+write(STDERR_FILENO, av[1], strlen(av[1]));
+write(STDERR_FILENO, "\n", 1);
+exit(127);
 }
-/* to get what user types */
-/* getline(&typed, &m, stdin); */
-printf("%s\n", typed);
-free(typed);
+return (EXIT_FAILURE);
 }
-return (0);
+info->readfd = fd;
+}
+populate_env_list(info);
+read_history(info);
+hsh(info, av);
+return (EXIT_SUCCESS);
 }
--- shell.h
+++ shell.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int bc, char **argv);
+int main(int ac, char **av);
