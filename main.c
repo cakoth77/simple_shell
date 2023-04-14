4635a0aec935b4c70485fcc3415e11a19104835f
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-04-14 04:19:02 -0700

    simple shell

--- main.c
+++ main.c
@@ -6,19 +6,20 @@
  * @argv: arguments
  * Return: Always 0
  */
-int main(int bc, char **argv)
+int main(int ac, char **argv)
 {
-char *prompt = "(simpleshell) $ ";
+	char *prompt = "(simpleshell) $ ";
 /* variables for typed info and size allocation */
-char *typed;
-size_t m = 0;
+	char *lineptr;
+	size_t m = 0;
 /* void variables */
-(void)bc;
-(void)argv;
-printf("%s", prompt);
+	(void)ac;
+	(void)argv;
+	printf("%s", prompt);
 /* to get what user types */
-getline(&typed, &m, stdin);
-free(typed);
-return (0);
+	getline(&lineptr, &m, stdin);
+	printf("%s", lineptr);
+	free(lineptr);
+	return (0);
 }
 
