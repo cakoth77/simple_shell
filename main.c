2139e0c56dcdaf13ad4fd91232121a536c0b59c4
KerynMuchesia <121609173+KerynMuchesia@users.noreply.github.com>
2023-04-26 19:06:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include "main.h"
 /**
- * execmd - executes arguments
- * @argv: argument
- * Return: Always 0
- */
-void execmd(char **argv);
-/**
-<<<<<<< HEAD
  * main - entry point
  * Return: Always 0
-=======
  * main - writes a string to the standard output stream
  * Return: count
->>>>>>> f11b0b3 (simple_shell)
  */
+void execmd(char **argv);
 int main(void)
 {
 char *prompt = "(Wood) $ ";
@@ -25,17 +17,14 @@ int num_tokens = 0;
 char *token;
 int i;
 char **argv;
-
-/*creating a loop for the prompt*/
+  
 while (1)
 {
-/*printing the prompt character by character*/
 for (i = 0; prompt[i] != '\0'; i++)
 {
 our_putchar(prompt[i]);
 }
 read_chars = getline(&lineptr, &m, stdin);
-/*checking if the getline function failed or reached EOF or CTRL + D*/
 if (read_chars == -1)
 {
 for (i = 0; "Exiting....\n"[i] != '\0'; i++)
@@ -44,23 +33,17 @@ our_putchar("Exiting....\n"[i]);
 }
 break;
 }
-/*checking if the user input is "Exiting...."*/
 if (our_strcmp(lineptr, "Exiting....\n") == 0)
 {
 break;
 }
-/*allocating space for a copy of the lineptr */
 lineptr_copy = malloc(sizeof(char) * (read_chars + 1));
 if (lineptr_copy == NULL)
 {
 perror("tsh: memory allocation error");
 return (-1);
 }
-/*copying the lineptr to lineptr_copy*/
 our_strcpy(lineptr_copy, lineptr);
-
-/** splitting the string (lineptr) into an array of words **/
-/*calculating the total number of tokens*/
 token = strtok(lineptr, delim);
 
 while (token != NULL)
@@ -69,14 +52,12 @@ num_tokens++;
 token = strtok(NULL, delim);
 }
 num_tokens++;
-/*Allocating space to hold the array of strings*/
 argv = malloc(sizeof(char *) * num_tokens);
 if (argv == NULL)
 {
 perror("tsh: memory allocation error");
 return (-1);
 }
-/*storing each token in the argv array*/
 token = strtok(lineptr_copy, delim);
 
 for (i = 0; token != NULL; i++)
@@ -86,10 +67,8 @@ our_strcpy(argv[i], token);
 token = strtok(NULL, delim);
 }
 argv[i] = NULL;
-/*execution command*/
 execmd(argv);
 }
-/*freeing up the allocated memory*/
 for (i = 0; i < num_tokens; i++)
 {
 free(argv[i]);
